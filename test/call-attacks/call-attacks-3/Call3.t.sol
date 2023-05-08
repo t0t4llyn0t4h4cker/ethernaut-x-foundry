299e9dfb430fa352094c053cc1f5ef3f79e78d2c
NeedMoreCoffee <t0t4llyn0t4h4cker@gmail.com>
2023-05-08 16:21:35 -0500

    CryptoKeeperFixed created with isLocked()

--- test/call-attacks/call-attacks-3/Call3.t.sol
+++ test/call-attacks/call-attacks-3/Call3.t.sol
@@ -2,7 +2,6 @@ pragma solidity 0.8.13;
 
 import "forge-std/Test.sol";
 import {usdcErc20} from "src/call-attacks/call-attacks-2/ERC20.sol";
-//
 import "../../../src/call-attacks/call-attacks-3/CryptoKeeper.sol";
 import "../../../src/call-attacks/call-attacks-3/CryptoKeeperFactory.sol";
 import "../../../src/call-attacks/call-attacks-3/ICryptoKeeper.sol";
